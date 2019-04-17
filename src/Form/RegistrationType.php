038e404e15934018fb9c68afb5950c1fc21ebe94
daliusbu <daliusbu@gmail.com>
2019-04-17 16:35:14 +0300

    User registration form: added firstName and lastName; registered new regisration service; settled DB connection

---
+++ src/Form/RegistrationType.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('firstName');
+        $builder->add('lastName');
+    }
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+    }
+    public function getBlockPrefix()
+    {
+        return 'app_user_registration';
+    }
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+}
