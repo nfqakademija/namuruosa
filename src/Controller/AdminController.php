dc6c0ef3e8e66a75ac079b575d16d326adcec363
Tadas Janca <tadasjanca@gmail.com>
2019-05-22 23:21:02 +0300

    Added delete button for categories

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -14,12 +14,15 @@ use App\Form\CategoryType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController {
 
     /**
      * @Route("/admin/category", name="admin_category")
+     * @param Request $request
+     * @return RedirectResponse|Response
      */
     public function adminHome(Request $request)
     {
@@ -27,21 +30,38 @@ class AdminController extends AbstractController {
             ->getRepository(Category::class)
             ->getAllCategories();
 
-        $form = $this->createForm(CategoryType::class);
-        $form->handleRequest($request);
+        $form = $this->createForm(CategoryType::class)
+            ->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()){
             $em = $this->getDoctrine()->getManager();
-            $category = $form->getData();
-            $em->persist($category);
+            $newCategory = $form->getData();
+            $em->persist($newCategory);
             $em->flush();
-            return $this->redirectToRoute('my_jobs');
+            return $this->redirectToRoute('admin_category');
         }
-
         return $this->render('admin/categories.html.twig', [
             'categories' => $categories,
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @param $id
+     * @Route("admin/category/delete/{id}", name="admin_category_delete")
+     * @return RedirectResponse
+     */
+    public function delete($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $category = $em
+            ->getRepository(Category::class)
+            ->find($id);
+
+        $em->remove($category);
+        $em->flush();
+
+        return $this->redirectToRoute('admin_category');
     }
 
 }
