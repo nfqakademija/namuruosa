4012cc5317068106e95df4cc4834d9c496ea9595
Tadas Janca <tadasjanca@gmail.com>
2019-05-27 18:45:13 +0300

    delete button for matches

--- src/Controller/Admin/MatchesController.php
+++ src/Controller/Admin/MatchesController.php
@@ -6,6 +6,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Match;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class MatchesController extends AbstractController
 {
@@ -18,5 +19,22 @@ class MatchesController extends AbstractController
         return $this->render('admin/matches.html.twig', [
             'matches' => $matches,
         ]);
+    }
+
+    /**
+     * @param $id
+     * @return RedirectResponse
+     */
+    public function adminDeleteMatch($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $match = $em
+            ->getRepository(Match::class)
+            ->find($id);
+
+        $em->remove($match);
+        $em->flush();
+
+        return $this->redirectToRoute('admin_matches');
     }
 }
