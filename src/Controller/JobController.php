fc511223a83c39891b80810b1250d9ab22c6b475
daliusbu <daliusbu@gmail.com>
2019-05-28 22:34:41 +0300

    job content edit - finished

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -2,15 +2,30 @@
 
 namespace App\Controller;
 
-use App\Entity\Job;
 use App\Form\JobType;
 use App\Job\Loader;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class JobController extends AbstractController
 {
+
+    private $em;
+    private $loader;
+
+    /**
+     * JobController constructor.
+     * @param EntityManagerInterface $em
+     * @param Loader $loader
+     */
+    public function __construct(EntityManagerInterface $em, Loader $loader)
+    {
+        $this->em = $em;
+        $this->loader = $loader;
+    }
+
 
     /**
      * @Route("/job/add", name="jobAdd")
@@ -19,13 +34,11 @@ class JobController extends AbstractController
     {
         $form = $this->createForm(JobType::class);
         $form->handleRequest($request);
-
         if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
             $job = $form->getData();
             $job->setUserId($this->getUser());
-            $em->persist($job);
-            $em->flush();
+            $this->em->persist($job);
+            $this->em->flush();
             return $this->redirectToRoute('my_jobs');
         }
 
@@ -34,36 +47,34 @@ class JobController extends AbstractController
         ]);
     }
 
-
-    /**
+     /**
      * @Route("/job/edit/{id}", name="jobEdit")
+     * @param Request $request
+     * @param int $id
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
-    public function editJob( Request $request, int $id, Loader $loader)
+    public function editJob(Request $request, int $id)
     {
-        $job = $loader->getJob($id);
+        $job = $this->loader->getJob($id);
         $form = $this->createForm(JobType::class, $job);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $job = $form->getData();
-            $job->setUserId($this->getUser());
-            $em->persist($job);
-            $em->flush();
+            $this->em->flush();
             return $this->redirectToRoute('my_jobs');
         }
 
-        return $this->render('job/add.html.twig', [
+        return $this->render('job/edit.html.twig', [
             'form' => $form->createView(),
+            'id' => $job->getId(),
         ]);
     }
-
 
     /**
      * @Route("/job/delete/{jobId}", name="jobDelete")
      */
-    public function deleteJob($jobId, Request $request, Loader $loader)
+    public function deleteJob($jobId, Request $request)
     {
-        $loader->delete($jobId);
+        $this->loader->delete($jobId);
         $referer = $request->headers->get('referer');
         return $this->redirect($referer);
     }
@@ -72,10 +83,10 @@ class JobController extends AbstractController
      *
      * @Route("job/myjobs", name="my_jobs")
      */
-    public function listMyJobs(Loader $loader)
+    public function listMyJobs()
     {
         $userId = $this->getUser()->getId();
-        $myJobs = $loader->loadByUser($userId);
+        $myJobs = $this->loader->loadByUser($userId);
 
         return $this->render('job/my-jobs.html.twig', [
             'jobs' => $myJobs,
@@ -85,10 +96,10 @@ class JobController extends AbstractController
     /**
      * @Route("job/pot-matches", name="job_pot_matches")
      */
-    public function listPotMatches(Loader $loader)
+    public function listPotMatches()
     {
         $userId = $this->getUser()->getId();
-        $myMatchingServices = $loader->loadPotMatches($userId);
+        $myMatchingServices = $this->loader->loadPotMatches($userId);
 
         return $this->render('job/pot-matches.html.twig', [
             'potMatchesByJobs' => $myMatchingServices,
